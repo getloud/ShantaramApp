1ba89fdd819eb167c733e08468ab28fe3b056a73
oleksandr.lezvinskyi <lezvinskii@gmail.com>
2012-09-11 13:37:08 +0300

    second commit

---
+++ src/com/lezv/LunchList.java
@@ -0,0 +1,29 @@
+package com.lezv;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
+
+public class LunchList extends Activity {
+    Restaurant r = new Restaurant();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Button save = (Button)findViewById(R.id.save);
+        save.setOnClickListener(onSave);
+    }
+
+    private View.OnClickListener onSave=new View.OnClickListener() {
+        public void onClick(View v) {
+            EditText name=(EditText)findViewById(R.id.name);
+            EditText address=(EditText)findViewById(R.id.addr);
+            r.setName(name.getText().toString());
+            r.setAddress(address.getText().toString());
+        }
+    };
+}
---
+++ src/com/lezv/Restaurant.java
@@ -0,0 +1,29 @@
+package com.lezv;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: oleksandr.lezvinskyi
+ * Date: 9/11/12
+ * Time: 1:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Restaurant {
+    private String name = "";
+    private String address = "";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
