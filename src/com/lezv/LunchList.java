8fdea41dd2c6fbd43a30563cd0105e9e2818984f
Oleksandr Lezvinskii <lezvinskii@gmail.com>
2012-09-21 16:22:27 +0300

    tutorial 4

--- src/com/lezv/LunchList.java
+++ src/com/lezv/LunchList.java
@@ -2,6 +2,7 @@ package com.lezv;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.*;
 import android.view.View;
@@ -24,10 +25,8 @@ public class LunchList extends Activity {
         Button save = (Button)findViewById(R.id.save);
         save.setOnClickListener(onSave);
 
-        Spinner list=(Spinner)findViewById(R.id.restaurants);
-        adapter=new ArrayAdapter<Restaurant>(this,
-                android.R.layout.simple_spinner_item,
-                model);
+        ListView list=(ListView)findViewById(R.id.restaurants);
+        adapter=new RestaurantAdapter();
         list.setAdapter(adapter);
 
 
@@ -63,6 +62,55 @@ public class LunchList extends Activity {
     };
 
 
+    public class RestaurantAdapter extends ArrayAdapter<Restaurant> {
+        RestaurantAdapter() {
+            super(LunchList.this, android.R.layout.simple_list_item_1, model);
+
+
+        }
+
+        public View getView(int position, View convertView,
+                            ViewGroup parent) {
+            View row=convertView;
+            RestaurantHolder holder=null;
+            if (row==null) {
+                LayoutInflater inflater=getLayoutInflater();
+                row=inflater.inflate(R.layout.row, parent, false);
+                holder=new RestaurantHolder(row);
+                row.setTag(holder);
+            }
+            else {
+                holder=(RestaurantHolder)row.getTag();
+            }
+            holder.populateFrom(model.get(position));
+            return(row);
+        }
+    }
+
+
+    static class RestaurantHolder {
+        private TextView name=null;
+        private TextView address=null;
+        private ImageView icon=null;
+        RestaurantHolder(View row) {
+            name=(TextView)row.findViewById(R.id.title);
+            address=(TextView)row.findViewById(R.id.address);
+            icon=(ImageView)row.findViewById(R.id.icon);
+        }
+        void populateFrom(Restaurant r) {
+            name.setText(r.getName());
+            address.setText(r.getAddress());
+            if (r.getType().equals("sit_down")) {
+                icon.setImageResource(R.drawable.ball_red);
+            }
+            else if (r.getType().equals("take_out")) {
+                icon.setImageResource(R.drawable.ball_yellow);
+            }
+            else {
+                icon.setImageResource(R.drawable.ball_green);
+            }
+        }
+       }
 
 
 
